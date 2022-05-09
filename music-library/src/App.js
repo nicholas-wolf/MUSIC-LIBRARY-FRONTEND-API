424f8df7696c8e0659efb0add57a67dd0570ffef
nicholas-wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-05-09 17:17:55 -0400

    Created Components folders for MusicTable and SearchBar + .jsx and .css files for MusicTable and SearchBar + Created table function for MusicTable Component

--- music-library/src/App.js
+++ music-library/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import BasicTable from "./Components/MusicTable/MusicTable";
+
+
 
 function App() {
+  
+
+    const [songs, setSongs] = useState([])
+
+    useEffect(() => {
+      getAllSongs();
+    }, [])
+
+    async function getAllSongs (){
+        let response = await axios.get('http://localhost:8000/api/music/');
+
+        console.log(response.data)
+        setSongs(response.data)
+    }
+
+   
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+          <div>                                         
+          <BasicTable parentEntries={songs}/>                
+          </div> 
   );
 }
 
---
+++ music-library/src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+
+
+
+
+export default function BasicTable(props) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell align="right">Artist</TableCell>
+            <TableCell align="right">Album</TableCell>
+            <TableCell align="right">Release Date</TableCell>
+            <TableCell align="right">Genre</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.parentEntries.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.title}
+              </TableCell>
+              <TableCell align="right">{row.artist}</TableCell>
+              <TableCell align="right">{row.album}</TableCell>
+              <TableCell align="right">{row.releaseDate}</TableCell>
+              <TableCell align="right">{row.genre}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
